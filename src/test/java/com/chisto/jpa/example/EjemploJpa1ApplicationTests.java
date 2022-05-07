d051388036cebf24b6fed5e7f26572dc3ca4135a
Don Chistorra <shadowshaoran@gmail.com>
2022-05-07 07:23:55 +0200

    Commit Inicial

---
+++ src/test/java/com/chisto/jpa/example/EjemploJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chisto.jpa.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
